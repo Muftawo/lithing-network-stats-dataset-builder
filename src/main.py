970e01164939b99fd6133ce67644e93b89b1708f
Muftawo <muftawo.omar@berkeley.edu>
2024-08-19 13:38:16 +0000

    refactor: main.py

--- src/main.py
+++ src/main.py
@@ -1,33 +1,52 @@
 import logging
+import os
 import sys
 
-from dataset_builder.builder import DatasetBuilderService
+from dataset_builder.builder import DatasetBuilder
 
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.FileHandler("logs/logs.log"),  
-        # logging.StreamHandler(sys.stdout),  # Log to the console
-    ],
-)
+# Constants for exit codes
+SUCCESS = 0
+FAILURE = 1
 
-logger = logging.getLogger(__name__)
+# Configurable logging level and file path
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+LOG_FILE_PATH = os.getenv("LOG_FILE_PATH", "logs/DatasetBuilderLogger.log")
 
-logger.info("BITCOIN LIGHTNING NETWORK STATS DATASET BUILDER")
-logger.info(" ")
+
+def configure_logging():
+    if not logging.getLogger().hasHandlers():
+        logging.basicConfig(
+            level=getattr(logging, LOG_LEVEL, logging.INFO),
+            format="%(asctime)s [%(levelname)s] %(message)s",
+            handlers=[
+                logging.FileHandler(LOG_FILE_PATH),
+                # logging.StreamHandler(sys.stdout),  # Uncomment for console logging
+            ],
+        )
+
+
+def run_sync(dataset_builder, logger):
+    try:
+        dataset_builder.sync()
+        logger.info("The dataset has been synced successfully")
+        return SUCCESS
+    except Exception as e:
+        logger.exception("An error occurred during dataset sync:")
+        return FAILURE
 
 
 def main():
-    try:
-        dataset_builder = DatasetBuilderService()
-        dataset_builder.sync()
-        logger.info("\nThe dataset has been synced successfully")
-        sys.exit(0)
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-        sys.exit(1)
+    configure_logging()
+    logger = logging.getLogger("DatasetBuilderLogger")
+
+    logger.info("BITCOIN LIGHTNING NETWORK STATS DATASET BUILDER")
+    logger.info(" ")
+
+    dataset_builder = DatasetBuilder()
+
+    # Run the sync and return the exit code
+    exit_code = run_sync(dataset_builder, logger)
+    sys.exit(exit_code)
 
 
 if __name__ == "__main__":
